abc296c36b83dea4010a67562b0d3ef00ba072d6
David M. Lee <dlee@digium.com>
2013-10-21 15:02:53 -0500

    Closures, FTW!

--- python/ari_test/originate_example.py
+++ python/ari_test/originate_example.py
@@ -18,55 +18,23 @@ else:
     print "Created bridge %s" % holding_bridge.id
 
 
-def on_enter(bridge, ev):
-    # ignore announcer channels - see ASTERISK-22744
-    if ev['channel']['name'].startswith('Announcer/'):
-        return
+def connect(incoming, ev):
+    if ev['args'] == ['incoming']:
+        incoming.answer()
+        incoming.play(media="sound:pls-wait-connect-call")
+        holding_bridge.addChannel(channel=incoming.id)
+        outgoing = client.originate(endpoint="SIP/blink", app="hello",
+                                    appArgs="dialed")
+        incoming.on_event('StasisEnd', lambda *args: outgoing.hangup())
+        outgoing.on_event('ChannelDestroyed', lambda *args: incoming.hangup())
+
+        def bridge_the_call(*ignored):
+            bridge = client.create_bridge(type='mixing')
+            outgoing.answer()
+            bridge.addChannel(channel=[incoming.id, outgoing.id])
+            outgoing.on_event('StasisEnd', lambda *args: bridge.destroy())
+        outgoing.on_event('StasisStart', bridge_the_call)
 
 
-class Call(object):
-    def __init__(self, incoming, outgoing):
-        self.incoming_id = incoming.id
-        self.outgoing_id = outgoing.id
-        self.bridge_id = None
-
-
-calls = []
-
-
-def hold_please(channel, ev):
-    if ev['args'] == ['dialed']:
-        call = [c for c in calls if c.outgoing_id == channel.id]
-        if len(call) != 1:
-            channel.hangup()
-            return
-        call = call[0]
-        channel.answer()
-        bridge = client.create_bridge(type='mixing')
-        call.bridge_id = bridge.id
-        bridge.addChannel(channel=[call.incoming_id, call.outgoing_id])
-    else:
-        channel.answer()
-        channel.play(media="sound:pls-wait-connect-call")
-        holding_bridge.addChannel(channel=channel.id)
-        dialed = client.originate(endpoint="SIP/blink", app="hello",
-                                  appArgs="dialed")
-        calls.append(Call(channel, dialed))
-
-
-def bye(channel, ev):
-    for call in calls:
-        if channel.id == call.incoming_id:
-            client.swagger.apis.channels.hangup(channelId=call.outgoing_id)
-        elif channel.id == call.outgoing_id:
-            client.swagger.apis.channels.hangup(channelId=call.incoming_id)
-        else:
-            continue
-
-        client.swagger.apis.bridges.destroy(bridgeId=call.bridge_id)
-
-
-client.on_channel_event('StasisStart', hold_please)
-client.on_channel_event('StasisEnd', bye)
-client.on_channel_event('ChannelDestroyed', bye)
+client.on_channel_event('StasisStart', connect)
 client.run()
