8b3e87532857c519cd456d7ea175c3c1f56ef5a6
David M. Lee <dlee@digium.com>
2013-10-23 12:09:50 -0500

    Test major cases I know could fail.

--- python/ari_test/client_test.py
+++ python/ari_test/client_test.py
@@ -7,6 +7,7 @@ import unittest
 import urllib
 import urlparse
 import ari
+import requests
 
 BASE_URL = "http://ari.py/ari"
 GET = httpretty.GET
@@ -24,24 +25,80 @@ def build_url(*args):
 
 def serve(method, *args, **kwargs):
     url = build_url(*args)
+    if kwargs.get('body') is None and 'status' not in kwargs:
+        kwargs['status'] = requests.codes.no_content
     return httpretty.register_uri(method, url, content_type="application/json",
                                   **kwargs)
 
 
 class ClientTest(unittest.TestCase):
     def test_docs(self):
-        url = "http://ari.py/ari/api-docs/resources.json"
-        fp = urllib.urlopen(url)
+        fp = urllib.urlopen("http://ari.py/ari/api-docs/resources.json")
         try:
             actual = json.load(fp)
             self.assertEqual(BASE_URL, actual['basePath'])
         finally:
             fp.close()
 
-    def test_listing(self):
+    def test_empty_listing(self):
         serve(GET, 'channels', body='[]')
         actual = self.uut.channels.list()
         self.assertEqual([], actual)
+
+    def test_one_listing(self):
+        serve(GET, 'channels', body='[{"id": "test-channel"}]')
+        serve(DELETE, 'channels', 'test-channel')
+
+        actual = self.uut.channels.list()
+        self.assertEqual(1, len(actual))
+        actual[0].hangup()
+
+    def test_play(self):
+        serve(GET, 'channels', 'test-channel', body='{"id": "test-channel"}')
+        serve(POST, 'channels', 'test-channel', 'play',
+              body='{"id": "test-playback"}')
+        serve(DELETE, 'playback', 'test-playback')
+
+        channel = self.uut.channels.get(channelId='test-channel')
+        playback = channel.play(media='sound:test-sound')
+        playback.stop()
+
+    def test_bad_resource(self):
+        try:
+            self.uut.i_am_not_a_resource.list()
+            self.fail("How did it find that resource?")
+        except AttributeError:
+            pass
+
+    def test_bad_param(self):
+        try:
+            self.uut.channels.list(i_am_not_a_param='asdf')
+            self.fail("How did it find that param?")
+        except TypeError:
+            pass
+
+    def test_bad_response(self):
+        serve(GET, 'channels', body='{"message": "This is just a test"}',
+              status=500)
+        try:
+            self.uut.channels.list()
+            self.fail("Should have thrown an exception")
+        except requests.HTTPError as e:
+            self.assertEqual(500, e.response.status_code)
+            self.assertEqual(
+                {"message": "This is just a test"}, e.response.json())
+
+    def test_endpoints(self):
+        serve(GET, 'endpoints',
+              body='[{"technology": "TEST", "resource": "1234"}]')
+        serve(GET, 'endpoints', 'TEST', '1234',
+              body='{"technology": "TEST", "resource": "1234"}')
+
+        endpoints = self.uut.endpoints.list()
+        self.assertEqual(1, len(endpoints))
+        endpoint = endpoints[0].get()
+        self.assertEqual('TEST', endpoint.json['technology'])
+        self.assertEqual('1234', endpoint.json['resource'])
 
     def setUp(self):
         super(ClientTest, self).setUp()
