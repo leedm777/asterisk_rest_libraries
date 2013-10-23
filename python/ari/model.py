f3987a8c429caeb9b03cb5ba457f2e1fe1499c6b
David M. Lee <dlee@digium.com>
2013-10-23 13:46:58 -0500

    Abstraction to handle the case for Endpoints.

--- python/ari/model.py
+++ python/ari/model.py
@@ -47,18 +47,37 @@ class Repository(object):
         return lambda **kwargs: promote(self.client, oper(**kwargs), oper.json)
 
 
+class ObjectIdentifier(object):
+    def get_params(self, obj_json):
+        raise NotImplementedError("Not implemented")
+
+    def id_as_str(self, obj_json):
+        raise NotImplementedError("Not implemented")
+
+
+class DefaultObjectIdentifier(ObjectIdentifier):
+    def __init__(self, param_name, id_field='id'):
+        self.param_name = param_name
+        self.id_field = id_field
+
+    def get_params(self, obj_json):
+        return {self.param_name: obj_json[self.id_field]}
+
+    def id_as_str(self, obj_json):
+        return obj_json[self.id_field]
+
+
 class BaseObject(object):
-    def __init__(self, client, api, as_json, param_name, event_reg,
-                 get_id=lambda json: json['id']):
+    def __init__(self, client, api, as_json, identifier, event_reg):
         self.client = client
         self.api = api
         self.json = as_json
-        self.id = get_id(as_json)
-        self.param_name = param_name
         self.event_reg = event_reg
+        self.identifier = identifier
+        self.id = identifier.id_as_str(as_json)
 
     def __repr__(self):
-        return "%s(%s) {%r}" % (self.__class__.__name__, self.id, self.json)
+        return "%s(%s)=%r" % (self.__class__.__name__, self.id, self.json)
 
     def __getattr__(self, item):
         oper = getattr(self.api, item)
@@ -69,7 +88,7 @@ class BaseObject(object):
 
         def promoter(**kwargs):
             # Add id to param list
-            kwargs[self.param_name] = self.id
+            kwargs.update(self.identifier.get_params(self.json))
             return promote(self.client, oper(**kwargs), oper.json)
 
         return promoter
@@ -89,33 +108,40 @@ class BaseObject(object):
 class Channel(BaseObject):
     def __init__(self, client, channel_json):
         super(Channel, self).__init__(
-            client, client.swagger.apis.channels, channel_json, 'channelId',
-            client.on_channel_event)
+            client, client.swagger.apis.channels, channel_json,
+            DefaultObjectIdentifier('channelId'), client.on_channel_event)
 
 
 class Bridge(BaseObject):
     def __init__(self, client, bridge_json):
         super(Bridge, self).__init__(
-            client, client.swagger.apis.bridges, bridge_json, 'bridgeId',
-            client.on_bridge_event)
+            client, client.swagger.apis.bridges, bridge_json,
+            DefaultObjectIdentifier('bridgeId'), client.on_bridge_event)
 
 
 class Playback(BaseObject):
     def __init__(self, client, playback_json):
         super(Playback, self).__init__(
-            client, client.swagger.apis.playback, playback_json, 'playbackId',
-            client.on_playback_event)
+            client, client.swagger.apis.playback, playback_json,
+            DefaultObjectIdentifier('playbackId'), client.on_playback_event)
 
 
-def endpoint_get_id(json):
-    return "%s/%s" % (json['technology'], json['resource'])
+class EndpointIdentifier(ObjectIdentifier):
+    def get_params(self, obj_json):
+        return {
+            'tech': obj_json['technology'],
+            'resource': obj_json['resource']
+        }
+
+    def id_as_str(self, obj_json):
+        return "%(tech)s/%(resource)s" % self.get_params(obj_json)
 
 
 class Endpoint(BaseObject):
     def __init__(self, client, endpoint_json):
         super(Endpoint, self).__init__(
-            client, client.swagger.apis.endpoints, endpoint_json, 'endpointId',
-            client.on_endpoint_event, get_id=endpoint_get_id)
+            client, client.swagger.apis.endpoints, endpoint_json,
+            EndpointIdentifier(), client.on_endpoint_event)
 
 
 class Sound(BaseObject):
